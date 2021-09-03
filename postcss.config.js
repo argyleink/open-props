94a8b6c076ccfb02c92303d77a3d6f6946802cc5
Adam Argyle <atom@argyleink.com>
2021-09-02 23:07:58 -0700

    1st pass porting over prework

--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,16 @@ module.exports = {
     postcsseasings(),
     postcssPresetEnv({
       stage: 0,
+      autoprefixer: false,
       features: {
         'logical-properties-and-values': false, 
         'prefers-color-scheme-query': false, 
         'gap-properties': false,
         'custom-properties': false,
         'place-properties': false,
+        'not-pseudo-class': false,
+        'focus-visible-pseudo-class': false,
+        'focus-within-pseudo-class': false,
       }
     }),
   ]
