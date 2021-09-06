9368ef1838287a758d3f18a8d852e77e325f92cd
Adam Argyle <atom@argyleink.com>
2021-09-05 22:14:40 -0700

    adds colors from open-colors project

---
+++ props.colors.src.js
@@ -0,0 +1,23 @@
+import openColor from 'https://cdn.skypack.dev/open-color/open-color.js'
+
+const colors = Object
+  .entries(openColor.theme.colors)
+  .filter(group => typeof group[1] === 'object')
+
+const customizeIncrements = num =>
+  num === '50'
+    ? num
+    : num.replaceAll('0', '')
+
+const vars = colors.reduce((root, [color, shades]) => {
+  let base = `--${color}-`
+
+  Object.entries(shades).forEach(([num, hex]) => 
+    root += `
+    ${base}${customizeIncrements(num)}: ${hex};`
+  )
+
+  return root
+}, ``)
+
+console.log(vars)
