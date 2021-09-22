e40fca52d31a63c73dbe87a224e07571ed0e8971
Adam Argyle <argyle@google.com>
2021-09-22 11:50:33 -0700

    adds prototype for pausing/playing easings

--- docsite/index.js
+++ docsite/index.js
@@ -1,3 +1,14 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/prism.min.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/line-numbers/prism-line-numbers.min.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
+
+document
+  .querySelectorAll('.animation-control-header > button')
+  .forEach(button => {
+    button.addEventListener('click', e => {
+      e.currentTarget
+        .closest('div')
+        .querySelector('.ease-demo')
+        .classList.toggle('paused')
+    })
+  })
