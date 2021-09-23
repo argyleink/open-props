a19f48f829b89238678bfa0499adf18325aabd97
Adam Argyle <atom@argyleink.com>
2021-09-22 23:01:22 -0700

    adds font size demos

--- docsite/index.js
+++ docsite/index.js
@@ -45,3 +45,24 @@ document
       sample.addEventListener('animationend', cleanup, {once:true})
     }
   })
+
+// slider
+const sliders = document.querySelectorAll('input[type="range"]')
+
+const rangeToPercent = slider => {
+  const max = slider.getAttribute('max') || 10
+  const percent = slider.value / max * 100
+
+  return `${parseInt(percent)}%`
+}
+
+sliders.forEach(slider => {
+  slider.style.setProperty('--track-fill', rangeToPercent(slider))
+
+  slider.addEventListener('input', e => {
+    e.target.style.setProperty('--track-fill', rangeToPercent(e.target))
+    e.target
+      .parentElement.querySelector('.sample-text')
+      .style[e.target.dataset.prop] = `var(--font-${e.target.name}-${e.target.value})`
+  })
+})
