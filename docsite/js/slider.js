8bb69176cee63eb36dbfc19b181b7ede24a65bfc
Adam Argyle <atom@argyleink.com>
2021-11-02 21:14:38 -0700

    fixes #45

--- docsite/js/slider.js
+++ docsite/js/slider.js
@@ -1,19 +1,50 @@
 const sliders = document.querySelectorAll('input[type="range"]')
 
 const rangeToPercent = slider => {
-  const max = slider.getAttribute('max') || 10
-  const percent = slider.value / max * 100
+  let percent
+  let val = slider.value
+  let max = slider.max
+
+  if (slider.min === '-1' && (val === "-1" || val === "0"))
+    percent = (val+1) / max * 100
+  else if (slider.min === '-1' && val === "1")
+    percent = val / (max-2) * 100
+  else
+    percent = val / max * 100
 
   return `${parseInt(percent)}%`
 }
 
+const highlightProperty = slider => {
+  if (slider.connectedProperty)
+    slider.connectedProperty.style.color = null
+    
+  let offset = parseInt(slider.value)
+
+  if (slider.min === '1') offset -= 1
+  if (slider.min === '-1') offset += 1
+
+  slider.connectedProperty = slider.parentElement.parentElement
+    .querySelectorAll(`code > .property`)[offset]
+  slider.connectedProperty.style.color = 'white'
+}
+
 sliders.forEach(slider => {
   slider.style.setProperty('--track-fill', rangeToPercent(slider))
+  // todo: wait for prism to tokenize code blocks
+  // or figure out how to prerender prism blocks
+  // highlightProperty(slider)
 
   slider.addEventListener('input', e => {
-    e.target.style.setProperty('--track-fill', rangeToPercent(e.target))
-    e.target
+    slider.style.setProperty('--track-fill', rangeToPercent(slider))
+
+    let value = slider.value
+    if (value === '-1') value = '00'
+
+    slider
       .parentElement.querySelector('.sample-text')
-      .style[e.target.dataset.prop] = `var(--font-${e.target.name}-${e.target.value})`
+      .style[slider.dataset.prop] = `var(--font-${slider.name}-${value})`
+
+    highlightProperty(slider)
   })
 })
