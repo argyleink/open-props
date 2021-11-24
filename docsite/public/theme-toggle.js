e1f245ef75b68c805a91695f59adb799ed068dd7
Adam Argyle <atom@argyleink.com>
2021-11-23 21:24:43 -0800

    migration foundation

--- docsite/public/theme-toggle.js
+++ docsite/public/theme-toggle.js
@@ -16,38 +16,10 @@ const setPreference = () => {
 const reflectPreference = () => {
   document.firstElementChild.setAttribute('data-theme', theme.value)
   document.querySelector('#theme-toggle')?.setAttribute('aria-live', theme.value)
-
-  setCodeSnippetStylesheet()
-}
-
-const setCodeSnippetStylesheet = () => {
-  if (theme.value === 'dark') {
-    if (document.head.querySelector('link#lightcode'))
-      document.head.removeChild(document.head.querySelector('link#lightcode'))
-    document.head.appendChild(theme.prismDark)
-  }
-  else {
-    if (document.head.querySelector('link#darkcode'))
-      document.head.removeChild(document.head.querySelector('link#darkcode'))
-    document.head.appendChild(theme.prismLight)  
-  }
-}
-
-const makeLinkStylesheet = (id, href) => {
-  const link = document.createElement('link')
-
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  link.href = href
-  link.id = id
-
-  return link
 }
 
 const theme = {
   value: getColorPreference(),
-  prismDark: makeLinkStylesheet('darkcode', '//unpkg.com/prism-themes@1.8.0/themes/prism-nord.css'),
-  prismLight: makeLinkStylesheet('lightcode', '//unpkg.com/prism-themes@1.8.0/themes/prism-material-light.css'),
 }
 
 // set early so no page flashes
