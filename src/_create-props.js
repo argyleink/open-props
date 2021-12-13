85d3461b6f0ef3945e9b35aafb2383bb85de6abc
Adam Argyle <atom@argyleink.com>
2021-12-12 21:17:36 -0800

    fixes #111

--- src/_create-props.js
+++ src/_create-props.js
@@ -57,8 +57,20 @@ const jsonbundle = {
   ...Borders,
 }
 const designtokens = Object.entries(jsonbundle).map(([key, token]) => {
+  let meta = {}
+
+  let isLength = key.includes('size')
+  let isEasing = key.includes('ease')
+  let colors = ['gray','red','pink','grape','violet','indigo','blue','cyan','teal','green','lime','yellow','orange']
+  let isColor = colors.some(color => key.includes(color))
+
+  if      (isLength) meta.type = 'dimension'
+  else if (isEasing) meta.type = 'cubic-bezier'
+  else if (isColor)  meta.type = 'color'
+
   return [key, {
-    value: token
+    value: token,
+    ...meta,
   }]
 })
 
