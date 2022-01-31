42736e59bc8416bb6838f337741b26cd375c108a
Adam Argyle <atom@argyleink.com>
2022-01-30 21:47:31 -0800

    gen hsl channels for each hex

--- src/props.colors.src.js
+++ src/props.colors.src.js
@@ -1,4 +1,5 @@
-import openColor from 'https://cdn.skypack.dev/open-color/open-color.js'
+const openColor = (await import('https://cdn.skypack.dev/open-color/open-color.js')).default
+const Color = (await import('https://colorjs.io/dist/color.esm.js')).default
 
 const colors = Object
   .entries(openColor.theme.colors)
@@ -9,10 +10,20 @@ const customizeIncrements = num =>
     ? num.replaceAll('50', '0')
     : num.replaceAll('0', '')
 
+const hexTOhsl = hex =>
+  new Color(hex).to('hsl')
+    .coords.map(Math.round)
+    .reduce((acc, coord, index) => {
+      if (index > 0)
+        return acc += ' ' + coord + '%'
+      else
+        return acc += coord
+    }, '')
+
 const capitalizeFirstLetter = string =>
   string.charAt(0).toUpperCase() + string.slice(1);
 
-const vars = colors.reduce((root, [color, shades]) => {
+const groupedObject = colors.reduce((root, [color, shades]) => {
   let base = `--${color}-`
   root += `\n\nconst ${capitalizeFirstLetter(color)} = {`
 
@@ -26,4 +37,27 @@ const vars = colors.reduce((root, [color, shades]) => {
   return root
 }, ``)
 
-console.log(vars)
+const channels = colors.reduce((root, [color, shades]) => {
+  let base = `--${color}-`
+
+  Object.entries(shades).forEach(([num, hex]) => 
+    root += `
+    ${base}${customizeIncrements(num)}-hsl: '${hexTOhsl(hex)}',`
+  )
+
+  return root
+}, ``)
+
+const vars = colors.reduce((root, [color, shades]) => {
+  let base = `--${color}-`
+
+  Object.entries(shades).forEach(([num, hex]) => 
+    root += `
+    ${base}${customizeIncrements(num)}: ${hex};`
+  )
+
+  return root
+}, ``)
+
+// console.log(vars)
+console.log(channels)
