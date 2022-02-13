d779b9b16a2cd5045e1d2d1d65a9bc3f15773363
Adam Argyle <atom@argyleink.com>
2022-02-12 22:24:38 -0800

    fixes #189

--- test/basic.test.cjs
+++ test/basic.test.cjs
@@ -3,20 +3,23 @@ const OpenProps = require('../dist/open-props.cjs')
 const OPtokens = require('../open-props.tokens.json')
 
 test('Should have an all included import', t => {
-  t.is(Object.keys(OpenProps).length, 505)
+  t.is(Object.keys(OpenProps).length, 1010)
 })
 
 test('Import should have animations', async t => {
   t.assert(Object.keys(OpenProps).includes('--animation-fade-in'))
+  t.assert(OpenProps.animationFadeIn)
   t.assert(Object.keys(OpenProps).includes('--animation-fade-in-@'))
 })
 
 test('Import should have sizes', async t => {
   t.assert(Object.keys(OpenProps).includes('--size-5'))
+  t.assert(OpenProps.size5)
 })
 
 test('Import should have colors', async t => {
   t.assert(Object.keys(OpenProps).includes('--orange-0'))
+  t.assert(OpenProps.orange0)
 })
 
 test('JSON Import should have colors', async t => {
