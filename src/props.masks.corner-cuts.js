edf08848c0519fb80c33c2824a17c16952cc0580
Adam Argyle <atom@argyleink.com>
2022-03-22 21:17:17 -0700

    updates masks with latest from temani

--- src/props.masks.corner-cuts.js
+++ src/props.masks.corner-cuts.js
@@ -3,39 +3,33 @@ export default {
   '--mask-corner-cut-circles-2': 'radial-gradient(circle 2rem,#0000 99%,#000) 10000% 10000% / 99.5% 99.5%',
   '--mask-corner-cut-circles-3': 'radial-gradient(circle 4rem,#0000 99%,#000) 10000% 10000% / 99.5% 99.5%',
   '--mask-corner-cut-squares-1': `
-    conic-gradient(from calc(90deg / -2 -  45deg) at top    1rem left  1rem,#0000 90deg,#000 0) 0    0,
-    conic-gradient(from calc(90deg / -2 +  45deg) at top    1rem right 1rem,#0000 90deg,#000 0) 100% 0,
-    conic-gradient(from calc(90deg / -2 - 135deg) at bottom 1rem left  1rem,#0000 90deg,#000 0) 0    100%,
-    conic-gradient(from calc(90deg / -2 + 135deg) at bottom 1rem right 1rem,#0000 90deg,#000 0) 100% 100%
+    conic-gradient(from -90deg at 1rem 1rem,#0000 90deg,#000 0) 
+    0 0/calc(100%  - 1rem)   calc(100%  - 1rem)
   `,
   '--mask-corner-cut-squares-2': `
-    conic-gradient(from calc(90deg / -2 -  45deg) at top    2rem left  2rem,#0000 90deg,#000 0) 0    0,
-    conic-gradient(from calc(90deg / -2 +  45deg) at top    2rem right 2rem,#0000 90deg,#000 0) 100% 0,
-    conic-gradient(from calc(90deg / -2 - 135deg) at bottom 2rem left  2rem,#0000 90deg,#000 0) 0    100%,
-    conic-gradient(from calc(90deg / -2 + 135deg) at bottom 2rem right 2rem,#0000 90deg,#000 0) 100% 100%
+    conic-gradient(from -90deg at 2rem 2rem,#0000 90deg,#000 0) 
+    0 0/calc(100%  - 2rem)   calc(100%  - 2rem)
   `,
   '--mask-corner-cut-squares-3': `
-    conic-gradient(from calc(90deg / -2 -  45deg) at top    4rem left  4rem,#0000 90deg,#000 0) 0    0,
-    conic-gradient(from calc(90deg / -2 +  45deg) at top    4rem right 4rem,#0000 90deg,#000 0) 100% 0,
-    conic-gradient(from calc(90deg / -2 - 135deg) at bottom 4rem left  4rem,#0000 90deg,#000 0) 0    100%,
-    conic-gradient(from calc(90deg / -2 + 135deg) at bottom 4rem right 4rem,#0000 90deg,#000 0) 100% 100%
+    conic-gradient(from -90deg at 4rem 4rem,#0000 90deg,#000 0) 
+    0 0/calc(100%  - 4rem)   calc(100%  - 4rem)
   `,
   '--mask-corner-cut-angles-1': `
-    conic-gradient(from calc(180deg / -2 -  45deg) at top    1rem left  1rem,#0000 180deg,#000 0) 0    0,
-    conic-gradient(from calc(180deg / -2 +  45deg) at top    1rem right 1rem,#0000 180deg,#000 0) 100% 0,
-    conic-gradient(from calc(180deg / -2 - 135deg) at bottom 1rem left  1rem,#0000 180deg,#000 0) 0    100%,
-    conic-gradient(from calc(180deg / -2 + 135deg) at bottom 1rem right 1rem,#0000 180deg,#000 0) 100% 100%
+    conic-gradient(from -45deg at 1rem 1rem,#0000 90deg,#000 0) 
+    calc(-1*1rem) 0/100% 51% repeat-x,
+    conic-gradient(from 135deg at 1rem calc(100% - 1rem),#0000 90deg,#000 0) 
+    calc(-1*1rem) 100%/100% 51% repeat-x
   `,
   '--mask-corner-cut-angles-2': `
-    conic-gradient(from calc(180deg / -2 -  45deg) at top    2rem left  2rem,#0000 180deg,#000 0) 0    0,
-    conic-gradient(from calc(180deg / -2 +  45deg) at top    2rem right 2rem,#0000 180deg,#000 0) 100% 0,
-    conic-gradient(from calc(180deg / -2 - 135deg) at bottom 2rem left  2rem,#0000 180deg,#000 0) 0    100%,
-    conic-gradient(from calc(180deg / -2 + 135deg) at bottom 2rem right 2rem,#0000 180deg,#000 0) 100% 100%
+    conic-gradient(from -45deg at 2rem 2rem,#0000 90deg,#000 0) 
+    calc(-1*2rem) 0/100% 51% repeat-x,
+    conic-gradient(from 135deg at 2rem calc(100% - 2rem),#0000 90deg,#000 0) 
+    calc(-1*2rem) 100%/100% 51% repeat-x
   `,
   '--mask-corner-cut-angles-3': `
-    conic-gradient(from calc(180deg / -2 -  45deg) at top    4rem left  4rem,#0000 180deg,#000 0) 0    0,
-    conic-gradient(from calc(180deg / -2 +  45deg) at top    4rem right 4rem,#0000 180deg,#000 0) 100% 0,
-    conic-gradient(from calc(180deg / -2 - 135deg) at bottom 4rem left  4rem,#0000 180deg,#000 0) 0    100%,
-    conic-gradient(from calc(180deg / -2 + 135deg) at bottom 4rem right 4rem,#0000 180deg,#000 0) 100% 100%
+    conic-gradient(from -45deg at 4rem 4rem,#0000 90deg,#000 0) 
+    calc(-1*4rem) 0/100% 51% repeat-x,
+    conic-gradient(from 135deg at 4rem calc(100% - 4rem),#0000 90deg,#000 0) 
+    calc(-1*4rem) 100%/100% 51% repeat-x
   `,
 }
