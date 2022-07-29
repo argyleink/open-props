9c9b26aab4f75b7c34921bf2bdfb6a8233537ad3
Adam Argyle <argyle@google.com>
2022-07-29 04:31:57 +0000

    remove buttons, integrate with build

--- build/extras.js
+++ build/extras.js
@@ -7,13 +7,6 @@ readFile('./normalize.min.css', 'utf-8', (err, contents) => {
   writeFile('./normalize.shadow.min.css', replaced, 'utf-8', err => {
     console.log(err)
   })
-})
 
-readFile('./buttons.min.css', 'utf-8', (err, contents) => {
-  if (err) return console.log(err)
-  const replaced = contents.replace(/:where\(button\)/g, ':host')
-
-  writeFile('./buttons.shadow.min.css', replaced, 'utf-8', err => {
-    console.log(err)
-  })
+  process.exit(1)
 })
