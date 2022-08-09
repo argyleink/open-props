31c4b89f7ddb67eb5e62dd690d999b8200a829fa
Temani Afif <temani.afif@gmail.com>
2022-08-09 21:42:23 +0200

    Optimizing the Drips edges + minor updates (#259)
    * Update props.masks.edges.css
    
    * Update props.masks.edges.js

--- src/props.masks.edges.js
+++ src/props.masks.edges.js
@@ -20,8 +20,8 @@ export default {
     radial-gradient(circle farthest-side at bottom,#000 97%,#0000) top / calc(1.9 * 20px) 20px repeat-x
   `,
   '--mask-edge-scalloped-vertical': `
-    linear-gradient(#000 0 0) center / 100% calc(100% - 2 * 20px) no-repeat,
-    radial-gradient(circle farthest-side,#000 97%,#0000) center / calc(1.9 * 20px) calc(2 * 20px) repeat space
+    linear-gradient(#000 0 0) 50% / 100% calc(100% - 2 * 20px) no-repeat,
+    radial-gradient(circle farthest-side,#000 97%,#0000) 50% / calc(1.9 * 20px) calc(2 * 20px) repeat space
   `,
   '--mask-edge-scalloped-left': `
     linear-gradient(#000 0 0) right / calc(100% - 20px) 100% no-repeat,
@@ -32,42 +32,36 @@ export default {
     radial-gradient(circle farthest-side at left,#000 97%,#0000) right / 20px calc(1.9 * 20px) repeat-y
   `,
   '--mask-edge-scalloped-horizontal': `
-    linear-gradient(#000 0 0) center / calc(100% - 2 * 20px) 100% no-repeat,
-    radial-gradient(circle farthest-side,#000 97%,#0000) 0 / calc(2 * 20px) calc(1.9 * 20px) space repeat
+    linear-gradient(#000 0 0) 50% / calc(100% - 2 * 20px) 100% no-repeat,
+    radial-gradient(circle farthest-side,#000 97%,#0000) 50% / calc(2 * 20px) calc(1.9 * 20px) space repeat
   `,
   '--mask-edge-drip-bottom': `
-    linear-gradient(#000 0 0) top / 100% calc(100% - 2 * 20px) no-repeat,      
-    radial-gradient(circle farthest-side at bottom,#0000 97%,#000) 50% calc(100% - 20px)/calc(2 * 20px) 20px repeat-x,
-    radial-gradient(circle closest-side at 25% 50%,#000 97%,#0000) calc(50% - 20px) 99%/calc(2 * 2 * 20px) calc(2 * 20px) repeat-x
+    radial-gradient(20px at bottom,#0000 97%,#000) 50% calc(100% - 20px) / calc(2 * 20px) 100% repeat-x,
+    radial-gradient(20px at 25% 50%,#000 97%,#0000) calc(50% - 20px) 99% / calc(4 * 20px) calc(2 * 20px) repeat-x
   `,
   '--mask-edge-drip-top': `
-    linear-gradient(#000 0 0) bottom/100% calc(100% - 2 * 20px) no-repeat,      
-    radial-gradient(circle farthest-side at top,#0000 97%,#000) 50% 20px / calc(2 * 20px) 20px repeat-x,
-    radial-gradient(circle closest-side at 25% 50%,#000 97%,#0000) calc(50% - 20px) 1%/calc(4 * 20px) calc(2 * 20px) repeat-x
+    radial-gradient(20px at top,#0000 97%,#000) 50% 20px / calc(2 * 20px) 100% repeat-x,
+    radial-gradient(20px at 25% 50%,#000 97%,#0000) calc(50% - 20px) 1% / calc(4 * 20px) calc(2 * 20px) repeat-x
   `,
   '--mask-edge-drip-vertical': `
-    linear-gradient(#000 0 0) center / 100% calc(100% - 4 * 20px) no-repeat,      
-    radial-gradient(circle farthest-side at top,#0000 97%,#000) 50% 20px / calc(2 * 20px) 20px repeat-x,
-    radial-gradient(circle farthest-side at bottom,#0000 97%,#000) 50% calc(100% - 20px)/calc(2 * 20px) 20px repeat-x,
-    radial-gradient(circle closest-side at 25% 50%,#000 97%,#0000) calc(50% - 20px) 1%/calc(4 * 20px) calc(2 * 20px) repeat-x,
-    radial-gradient(circle closest-side at 25% 50%,#000 97%,#0000) calc(50% - 3*20px) 99%/calc(4 * 20px) calc(2 * 20px) repeat-x
+    radial-gradient(20px at top   ,#0000 97%,#000) 50%             20px  / calc(2 * 20px) 51% repeat-x,
+    radial-gradient(20px at bottom,#0000 97%,#000) 50% calc(100% - 20px) / calc(2 * 20px) 51% repeat-x,
+    radial-gradient(20px at 25% 50%,#000 97%,#0000) calc(50% -   20px) 1%  / calc(4 * 20px) calc(2 * 20px) repeat-x,
+    radial-gradient(20px at 25% 50%,#000 97%,#0000) calc(50% - 3*20px) 99% / calc(4 * 20px) calc(2 * 20px) repeat-x
   `,
   '--mask-edge-drip-left': `
-    linear-gradient(#000 0 0) right / calc(100% - 2 * 20px) 100% no-repeat,      
-    radial-gradient(circle farthest-side at left,#0000 97%,#000) 20px 50%/20px calc(2 * 20px) repeat-y,
-    radial-gradient(circle closest-side at 50% 25%,#000 97%,#0000) 1% calc(50% - 20px)/calc(2 * 20px) calc(4 * 20px) repeat-y
+    radial-gradient(20px at left,#0000 97%,#000) 20px 50% / 100% calc(2 * 20px) repeat-y,
+    radial-gradient(20px at 50% 25%,#000 97%,#0000) 1% calc(50% - 20px) / calc(2 * 20px) calc(4 * 20px) repeat-y
   `,
   '--mask-edge-drip-right': `
-    linear-gradient(#000 0 0) left / calc(100% - 2 * 20px) 100% no-repeat,      
-    radial-gradient(circle farthest-side at right,#0000 97%,#000) calc(100% - 20px) 50%/20px calc(2 * 20px) repeat-y,
-    radial-gradient(circle closest-side at 50% 25%,#000 97%,#0000) 99% calc(50% - 20px)/calc(2 * 20px) calc(4 * 20px) repeat-y
+    radial-gradient(20px at right,#0000 97%,#000) calc(100% - 20px) 50% / 100% calc(2 * 20px) repeat-y,
+    radial-gradient(20px at 50% 25%,#000 97%,#0000) 99% calc(50% - 20px) / calc(2 * 20px) calc(4 * 20px) repeat-y
   `,
   '--mask-edge-drip-horizontal': `
-    linear-gradient(#000 0 0) center / calc(100% - 4 * 20px) 100% no-repeat,      
-    radial-gradient(circle farthest-side at left,#0000 97%,#000) 20px 50%/20px calc(2 * 20px) repeat-y,  
-    radial-gradient(circle farthest-side at right,#0000 97%,#000) calc(100% - 20px) 50%/20px calc(2 * 20px) repeat-y,
-    radial-gradient(circle closest-side at 50% 25%,#000 97%,#0000) 1% calc(50% - 20px)/calc(2 * 20px) calc(4 * 20px) repeat-y,
-    radial-gradient(circle closest-side at 50% 25%,#000 97%,#0000) 99% calc(50% - 3*20px)/calc(2 * 20px) calc(4 * 20px) repeat-y
+    radial-gradient(20px at left ,#0000 97%,#000)             20px  50% / 51% calc(2 * 20px) repeat-y,  
+    radial-gradient(20px at right,#0000 97%,#000) calc(100% - 20px) 50% / 51% calc(2 * 20px) repeat-y,
+    radial-gradient(20px at 50% 25%,#000 97%,#0000) 1%  calc(50% -   20px) / calc(2 * 20px) calc(4 * 20px) repeat-y,
+    radial-gradient(20px at 50% 25%,#000 97%,#0000) 99% calc(50% - 3*20px) / calc(2 * 20px) calc(4 * 20px) repeat-y
   `,
   '--mask-edge-zig-zag-top': 'conic-gradient(from 135deg at top,#0000,#000 1deg 90deg,#0000 91deg) 50% / calc(2 * 20px) 100%',
   '--mask-edge-zig-zag-bottom': 'conic-gradient(from -45deg at bottom,#0000,#000 1deg 90deg,#0000 91deg) 50% / calc(2 * 20px) 100%',
