1dd9242afa30d5a1ce65f1ed6fd4ac1a478a6d1c
Ryan Mitchell <rmitchell756@gmail.com>
2023-09-29 15:08:16 -0400

    update number of props in test

--- test/basic.test.cjs
+++ test/basic.test.cjs
@@ -4,7 +4,7 @@ const OpenProps = require('../dist/open-props.cjs')
 const OPtokens = require('../open-props.tokens.json')
 
 test('Should have an all included import', t => {
-  t.is(Object.keys(OpenProps).length, 1612)
+  t.is(Object.keys(OpenProps).length, 1620)
 })
 
 test('Import should have animations', async t => {
