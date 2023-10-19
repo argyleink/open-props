d7226c3898c4fa9909209e823056d77b8969c97b
Adam Argyle <argyle@google.com>
2023-10-19 10:54:31 -0700

    update token count test

--- test/basic.test.cjs
+++ test/basic.test.cjs
@@ -24,7 +24,7 @@ test('Import should have colors', async t => {
 })
 
 test('JSON Import should have colors', async t => {
-  t.is(Object.keys(OPtokens).length, 434)
+  t.is(Object.keys(OPtokens).length, 439)
   t.assert(Object.keys(OPtokens).includes('--orange-0'))
 })
 
