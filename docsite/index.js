0e797e4fedbdc1bb2f2d8cf7cb79b5e3dee4a7e5
Brian-Pob <brianmp.dev@gmail.com>
2024-02-07 00:17:15 -0500

    add click to copy functionality

--- docsite/index.js
+++ docsite/index.js
@@ -6,3 +6,18 @@ import '/js/animations.js'
 import '/js/slider.js'
 import '/js/scrollspy.js'
 import '/js/select.js'
+
+function clickToCopy(style, property) {
+  navigator.clipboard.writeText(style[property]);
+  console.log(style[property])
+}
+
+function addCopyToColorSwatches() {
+  const colorSwatches = document.querySelectorAll('.color-swatch');
+  
+  colorSwatches.forEach((colorSwatch) => {
+    colorSwatch.addEventListener("click", () => clickToCopy(colorSwatch.style, "backgroundColor"))
+  });
+}
+
+window.onload = addCopyToColorSwatches;
