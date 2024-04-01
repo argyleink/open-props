35e01b33e8f37ceac0510cbf5331ad9259fd43ce
Arby <horizonmedimp@gmail.com>
2024-04-01 22:06:45 +0200

    Fix media queries '-only' breakpoints bug

--- src/props.media.js
+++ src/props.media.js
@@ -25,36 +25,36 @@ export const CustomMedia = {
   "--pointer": "(hover) and (pointer: coarse)",
   "--mouse": "(hover) and (pointer: fine)",
 
-  "--xxs-only": "(0px <= width <= 240px)",
+  "--xxs-only": "(0px <= width < 240px)",
   "--xxs-n-above": "(width >= 240px)",
   "--xxs-n-below": "(width < 240px)",
   "--xxs-phone": "(--xxs-only) and (--portrait)",
 
-  "--xs-only": "(240px <= width <= 360px)",
+  "--xs-only": "(240px <= width < 360px)",
   "--xs-n-above": "(width >= 360px)",
   "--xs-n-below": "(width < 360px)",
   "--xs-phone": "(--xs-only) and (--portrait)",
 
-  "--sm-only": "(360px <= width <= 480px)",
+  "--sm-only": "(360px <= width < 480px)",
   "--sm-n-above": "(width >= 480px)",
   "--sm-n-below": "(width < 480px)",
   "--sm-phone": "(--sm-only) and (--portrait)",
 
-  "--md-only": "(480px <= width <= 768px)",
+  "--md-only": "(480px <= width < 768px)",
   "--md-n-above": "(width >= 768px)",
   "--md-n-below": "(width < 768px)",
   "--md-phone": "(--md-only) and (--portrait)",
 
-  "--lg-only": "(768px <= width <= 1024px)",
+  "--lg-only": "(768px <= width < 1024px)",
   "--lg-n-above": "(width >= 1024px)",
   "--lg-n-below": "(width < 1024px)",
   "--lg-phone": "(--lg-only) and (--portrait)",
 
-  "--xl-only": "(1024px <= width <= 1440px)",
+  "--xl-only": "(1024px <= width < 1440px)",
   "--xl-n-above": "(width >= 1440px)",
   "--xl-n-below": "(width < 1440px)",
 
-  "--xxl-only": "(1440px <= width <= 1920px)",
+  "--xxl-only": "(1440px <= width < 1920px)",
   "--xxl-n-above": "(width >= 1920px)",
   "--xxl-n-below": "(width < 1920px)",
 }
