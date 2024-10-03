5b1673ba4513ce2424008ba32c419de647f5614b
aspizu <aspizu@protonmail.com>
2024-10-04 02:34:15 +0530

    Add showcase, code and demo for brand colors props to docsite.

---
+++ docsite/js/brand-color.js
@@ -0,0 +1,8 @@
+import brandProps from '../../src/props.brand.js'
+
+const brandColors = document.querySelector('#brand-colors')
+
+for (const [brand, color] of Object.entries(brandProps)) {
+  const brandTitlecase = brand.replace(/-/g, ' ').replace('brand ', '')
+  brandColors.innerHTML += `<ul class="open-colors count-em color-swatch-list"><h4>${brandTitlecase}</h4><li><button class="color-swatch" style="background-color: var(${brand})" aria-label="${brandTitlecase}"></button></li></ul>`
+}
