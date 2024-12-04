6a72f7082d37c69051983b0eeaeec43328f61f4d
Felix <felix.bohlin@protonmail.com>
2024-12-04 09:17:23 +0100

    feat(negative-sizes): add js files

---
+++ js/packs/negative-sizes.js
@@ -0,0 +1,54 @@
+export const NegativeSize = [
+	'-.25rem',
+	'-.5rem',
+	'-1rem',
+	'-1.25rem',
+	'-1.5rem',
+	'-1.75rem',
+	'-2rem',
+	'-3rem',
+	'-4rem',
+	'-5rem',
+	'-7.5rem',
+	'-10rem',
+	'-15rem',
+	'-20rem',
+	'-30rem',
+]
+
+export const NegativeRelative = [
+	'-.25ch',
+	'-.5ch',
+	'-1ch',
+	'-1.25ch',
+	'-1.5ch',
+	'-1.75ch',
+	'-2ch',
+	'-3ch',
+	'-4ch',
+	'-5ch',
+	'-7.5ch',
+	'-10ch',
+	'-15ch',
+	'-20ch',
+	'-30ch',
+]
+
+export const NegativeFluid = [
+	'clamp(-1rem, -1vw, -.5rem)',
+	'clamp(-1.5rem, -2vw, -1rem)',
+	'clamp(-2rem, -3vw, -1.5rem)',
+	'clamp(-3rem, -4vw, -2rem)',
+	'clamp(-5rem, -5vw, -4rem)',
+	'clamp(-7.5rem, -7vw, -5rem)',
+	'clamp(-10rem, -10vw, -7.5rem)',
+	'clamp(-15rem, -20vw, -10rem)',
+	'clamp(-20rem, -30vw, -15rem)',
+	'clamp(-30rem, -40vw, -20rem)',
+]
+
+export default {
+	NegativeSize,
+	NegativeRelative,
+	NegativeFluid,
+}
