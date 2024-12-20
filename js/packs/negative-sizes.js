2dcb12cdaaccbdf140173d573a55f078c97152a3
Felix <felix.bohlin@protonmail.com>
2024-12-20 22:03:45 +0100

    feat(negative-sizes): use calc approach

--- js/packs/negative-sizes.js
+++ js/packs/negative-sizes.js
@@ -1,51 +1,10 @@
-export const NegativeSize = [
-	'-.25rem',
-	'-.5rem',
-	'-1rem',
-	'-1.25rem',
-	'-1.5rem',
-	'-1.75rem',
-	'-2rem',
-	'-3rem',
-	'-4rem',
-	'-5rem',
-	'-7.5rem',
-	'-10rem',
-	'-15rem',
-	'-20rem',
-	'-30rem',
-]
+import { Fluid, Relative, Size } from '../sizes.js'
 
-export const NegativeRelative = [
-	'-.25ch',
-	'-.5ch',
-	'-1ch',
-	'-1.25ch',
-	'-1.5ch',
-	'-1.75ch',
-	'-2ch',
-	'-3ch',
-	'-4ch',
-	'-5ch',
-	'-7.5ch',
-	'-10ch',
-	'-15ch',
-	'-20ch',
-	'-30ch',
-]
+export const NegativeSize = Size.map((size) => `-${size}`)
 
-export const NegativeFluid = [
-	'clamp(-1rem, -1vi, -.5rem)',
-	'clamp(-1.5rem, -2vi, -1rem)',
-	'clamp(-2rem, -3vi, -1.5rem)',
-	'clamp(-3rem, -4vi, -2rem)',
-	'clamp(-5rem, -5vi, -4rem)',
-	'clamp(-7.5rem, -7vi, -5rem)',
-	'clamp(-10rem, -10vi, -7.5rem)',
-	'clamp(-15rem, -20vi, -10rem)',
-	'clamp(-20rem, -30vi, -15rem)',
-	'clamp(-30rem, -40vi, -20rem)',
-]
+export const NegativeRelative = Relative.map((size) => `-${size}`)
+
+export const NegativeFluid = Fluid.map((size) => `calc(-1 * ${size})`)
 
 export default {
 	NegativeSize,
